84ffd57003f3dd146e8e9cac2de81e55868a4096
Davilicus <florisslenderman@gmail.com>
2018-05-14 16:24:57 +0200

    Deelnemers routes added
    Added the beginning of the Deelnemers routes

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -10,4 +10,22 @@ router.get("/goodnight", (request, result) => {
     result.json("Goodnight World!");
 });
 
+router.post("/studentenhuis/:huisId?/maaltijd/:maaltijdId?", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId + ", " + maaltijdId);
+});
+
+router.get("/studentenhuis/:huisId?/maaltijd/:maaltijdId?/deelnemers", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId + ", " + maaltijdId);
+});
+
+router.delete("/studentenhuis/:huisId?/maaltijd/:maaltijdId?/deelnemers", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId + ", " + maaltijdId);
+});
+
 module.exports = router;
