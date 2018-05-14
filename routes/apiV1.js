af639124fe4098ca3016ec3f6951146563a0c1e9
Davilicus <florisslenderman@gmail.com>
2018-05-14 15:55:49 +0200

    Added routes
    Added the beginning of the routes for easy startup.

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -9,4 +9,28 @@ router.get("/hello", (request, result) => {
 router.get("/goodnight", (request, result) => {
     result.json("Goodnight World!");
 });
+
+router.get("/studentenhuis", (request, result) => {
+    result.json("Hello World!");
+});
+
+router.post("/studentenhuis", (request, result) => {
+    result.json("Hello World!");
+});
+
+router.get("/studentenhuis/:huisId?", (request, result) => {
+    const huisId = request.params.huisId;
+    result.json(huisId);
+});
+
+router.put("/studentenhuis/:huisId?", (request, result) => {
+    const huisId = request.params.huisId;
+    result.json(huisId);
+});
+
+router.delete("/studentenhuis/:huisId?", (request, result) => {
+    const huisId = request.params.huisId;
+    result.json(huisId);
+});
+
 module.exports = router;
