3220a6f6f67447d384475ed4a20c4611b3847ca6
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-05-14 16:23:30 +0200

    Added new meal routes

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -10,4 +10,36 @@ router.get("/goodnight", (request, result) => {
     result.json("Goodnight World!");
 });
 
+//Routes for Meals
+router.post("/studentenhuis/:huisId?/maaltijd", (request, result) => {
+    const huisId = request.params.huisId;
+    result.json(huisId);
+});
+
+router.get("/studentenhuis/:huisId?/maaltijd", (request, result) => {
+    const huisId = request.params.huisId;
+    result.json(huisId);
+});
+
+router.get("/studentenhuis/:huisId?/maaltijd/:maaltijdId?", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId);
+    result.json(maaltijdId);
+});
+
+router.put("/studentenhuis/:huisId?/maaltijd/:maaltijdId?", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId);
+    result.json(maaltijdId);
+});
+
+router.delete("/studentenhuis/:huisId?/maaltijd/:maaltijdId?", (request, result) => {
+    const huisId = request.params.huisId;
+    const maaltijdId = request.params.maaltijdId;
+    result.json(huisId);
+    result.json(maaltijdId);
+});
+
 module.exports = router;
