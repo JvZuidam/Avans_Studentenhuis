cd73b507397bc526d1f6e162303ff244e0038fd9
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-05-14 13:08:35 +0200

    Goodnight world routing branch test

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -6,4 +6,7 @@ router.get("/hello", (request, result) => {
     result.json("Hello World!");
 });
 
+router.get("/goodnight", (request, result) => {
+    result.json("Goodnight World!");
+});
 module.exports = router;
