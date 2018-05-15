51b2a2638ae65b3d89913fdc0ec4ae07fd7b6251
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-05-15 14:37:55 +0200

    Added body parser

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const router = express.Router();
+
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/hello", (request, result) => {
     result.json("Hello World!");
