649ac77b5959a1ddcef960a131e4f4fca65391a2
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-05-15 20:38:41 +0200

    Merge fix

--- database/db_Functions.js
+++ database/db_Functions.js
@@ -39,62 +39,69 @@ exports.Register = function(firstname, lastname, email, password) {
 };
 
 //Studentenhuis functions
-exports.newStudentenhuis = function(name, address, userId) {//
+exports.newStudentenhuis = function(name, address, userId, callback) {//
+
     //POST
     //Create new studentenhuis with email adn adress gotten from the json body request
-    connection.query("INSERT INTO studentenhuis (Naam, Adres, UserId) VALUES (" + name+ ", " + address + ", " + userId + ")", (err, rows, fields) => {
+    connection.query("INSERT INTO studentenhuis (Naam, Adres, UserId) VALUES ('" + name + "', '" + address + "', " + userId + ")", (err, rows, fields) => {
         if(err) {
             console.log('Error: ' + err)
         }
         if(rows) {
             console.log('We got rows!');
-            console.dir(rows)
+            console.log(rows);
+            console.log(rows.insertId);
+            callback(rows);
         }
     });
 };
 
-exports.getStudentenhuis = function () {
+exports.getStudentenhuis = function (callback) {
     //GET
     //Get all Studentenhuizen
-    connection.query("SELECT Naam, Adres FROM studentenhuis", (err, rows, fields) => {
+    connection.query("SELECT * FROM studentenhuis", (err, rows, fields) => {
         if(err) {
             console.log('Error: ' + err)
         }
         if(rows) {
-
+            console.log('We got rows!');
+            console.dir(rows)
+            callback(rows);
         }
     });
 };
 
-exports.getStudentenhuis = function (huisId) {
+exports.getStudentenhuisWithId = function (huisId, callback) {
     //GET
     //Get studentenhuis from huisId
-    connection.query("SELECT Naam, Adres FROM studentenhuis WHERE ID = " + huisId , (err, rows, fields) => {
+    connection.query("SELECT * FROM studentenhuis WHERE ID = " + huisId, (err, rows, fields) => {
         if(err) {
             console.log('Error: ' + err)
         }
         if(rows) {
             console.log('We got rows!');
-            console.dir(rows)
+            console.dir(rows);
+            callback(rows);
         }
     });
 };
 
-exports.updateStudentenhuis = function (name, address, huisId, userId) {
+exports.updateStudentenhuis = function (name, address, huisId, callback) {
     //PUT
     //Update studentenhuis from huisId
-    connection.query("UPDATE studentenhuis SET Naam = " + name + ", Adres = " + address + " WHERE ID = " + huisId + " AND UserId = " + userId, (err, rows, fields) => {
+    connection.query("UPDATE studentenhuis SET Naam = '" + name + "', Adres = '" + address + "' WHERE ID = " + huisId + " AND UserId = " + userId, (err, rows, fields) => {
         if(err) {
             console.log('Error: ' + err)
         }
         if(rows) {
             console.log('We got rows!');
-            console.dir(rows)
+            console.dir(rows);
+            callback(rows);
         }
     });
 };
 
-exports.deleteStudentenhuis = function () {
+exports.deleteStudentenhuis = function (huisId, userId, callback) {
     //DELETE
     //Delete studentenhuis from huisId
     connection.query("DELETE FROM studentenhuis WHERE ID = " + huisId + " AND UserId = " + userId, (err, rows, fields) => {
@@ -103,7 +110,8 @@ exports.deleteStudentenhuis = function () {
         }
         if(rows) {
             console.log('We got rows!');
-            console.dir(rows)
+            console.dir(rows);
+            callback(rows);
         }
     });
 };
