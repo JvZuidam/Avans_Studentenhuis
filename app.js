b63b8f399cd7f0a1a9b574a9e1602397ea71a028
Please don't visit my repositories <jimvanzuidam@hotmail.nl>
2018-05-16 13:45:38 +0200

    Port change and extra code

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("*", (req, res, next) => {
     next();
 });
 
+app.listen(process.env.PORT || 8088, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 app.use("/api", require("./routes/apiV1"));
 
 app.get("*", (req, res) => {
@@ -14,7 +18,7 @@ app.get("*", (req, res) => {
 });
 
 
-const port = 8080;
+const port = 8088;
 app.listen(port, () => {
     console.log("The magic happens at port " + port);
 });
