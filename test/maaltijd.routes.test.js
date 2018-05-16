11353e43269770f77d187c1ed2a743be34d2c798
Davilicus <florisslenderman@gmail.com>
2018-05-16 21:14:18 +0200

    Added test framework
    Added the frameworks for the tests, and a beginning to the frameworks of
    maaltijd en deelnemer

---
+++ test/maaltijd.routes.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Maaltijd API Post", () => {
+    it()
+});
